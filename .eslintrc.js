dd0e74ea044160e174fc4559e2f26d8bcd0994b3
sunasra <sunasra@gmail.com>
2018-11-15 19:37:40 +0530

    fist setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,87 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
   },
   plugins: [
     'ember'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
+    mocha: true
   },
   rules: {
+    "new-parens": "error",
+    "no-bitwise": 2,
+    "camelcase": [
+      1,
+      {
+        "properties": "never"
+      }
+    ],
+    "curly": [
+      1,
+      "all"
+    ],
+    "eqeqeq": 2,
+    "guard-for-in": 2,
+    "no-extend-native": 2,
+    "no-caller": 2,
+    "no-empty": 2,
+    "no-irregular-whitespace": 2,
+    "quotes": [
+      2,
+      "single"
+    ],
+    "no-undef": 2,
+    "no-unused-vars": 2,
+    "complexity": 2,
+    "max-len": [
+      1,
+      {
+        "code": 120,
+        "ignoreComments": true
+      }
+    ],
+    "semi": 2,
+    "no-eval": 2,
+    "no-lone-blocks": 2,
+    "comma-style": [
+      2
+    ],
+    "no-shadow": 2,
+    "dot-notation": 2,
+    "indent": [
+      "error",
+      4,
+      {
+        "SwitchCase": 1
+      }
+    ],
+    "consistent-this": [
+      2,
+      "that",
+      "self"
+    ],
+    "one-var-declaration-per-line": 2,
+    "key-spacing": 2,
+    "comma-dangle": 2,
+    "comma-spacing": 2,
+    "eol-last": 2,
+    "no-lonely-if": 2,
+    "no-alert": 2,
+    "no-unreachable": 2,
+    "max-statements": [1, 40],
+    "no-trailing-spaces": 1,
+    "no-multi-spaces": 2,
+    "no-multiple-empty-lines": 2
   },
   overrides: [
     // node files
@@ -38,9 +106,10 @@ module.exports = {
         sourceType: 'script',
         ecmaVersion: 2015
       },
-      env: {
-        browser: false,
-        node: true
+      "env": {
+        "browser": false,
+        "node": true,
+        "jquery": true,
       },
       plugins: ['node'],
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,43 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const nodeSass = require('node-sass');
 
-module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
+module.exports = function (defaults) {
+    let app = new EmberAddon(defaults, {
     // Add options here
-  });
+    // Add options here
+        babel: {
+            plugins: ['transform-object-rest-spread']
+        },
+        sassOptions: {
+            nodeSass: nodeSass,
+            includePaths: ['addon']
+        },
+        sassLint: {
+            configPath: '.sass-lint.yml',
+            shouldThrowExceptions: true,
+            shouldLog: true
+        },
+        svgJar: {
+            sourceDirs: [
+                'node_modules/feather-icons/dist/icons',
+                'public/assets/images/'
+            ]
+        }
+    });
 
-  /*
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
+    app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+    // app.import('bower_components/lodash/lodash.js');
+
+    /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
+    return app.toTree();
 };
