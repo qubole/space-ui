b646c6e42e9d0521b8796330fd3416d6b71834f6
Lalit Indoria <l.k.indoria@gmail.com>
2019-09-24 11:41:54 +0530

    Button enhancements

--- addon/components/s-button.js
+++ addon/components/s-button.js
@@ -4,5 +4,22 @@ import layout from '../templates/components/s-button';
 
 export default Component.extend(SpreadMixin, {
     classNames: ['s-button'],
-    layout
+    layout,
+    actions: {
+      handleClick() {
+        if (this.get('onClick')) {
+          this.get('onClick')();
+        }
+      },
+      onMouseEnter() {
+        if (this.get('onMouseEnter')) {
+          this.get('onMouseEnter')();
+        }
+      },
+      onMouseLeave() {
+        if (this.get('onMouseLeave')) {
+          this.get('onMouseLeave')();
+        }
+      }
+    }
 });
