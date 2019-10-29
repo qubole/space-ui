1c64d7a62882cbd9f7f159147ab4b1a5e246d1a3
Aaditya Sharma <asharma@qubole.com>
2019-10-29 23:00:56 +0530

    Added type for s-tag

--- addon/components/s-tag.js
+++ addon/components/s-tag.js
@@ -1,15 +1,17 @@
 import Component from '@ember/component';
-import layout from '../templates/components/s-tag';
 
 export default Component.extend({
-    layout,
-    tagName: 'span',
-    classNames: ['s-tag', 'container', 'mr5', 'fs16'],
-    actions: {
-        onClear() {
-            if (this.get('onClose')) {
-                this.get('onClose')(this.get('key'));
-            }
-        }
+  tagName: 'span',
+  classNames: ['s-tag', 'container', 'mr5', 'fs16'],
+  classNameBindings: ['getType'],
+  getType: Ember.computed('type', function(){
+    return this.get('type');
+  }),
+  actions: {
+    onClear() {
+      if (this.get('onClose')) {
+        this.get('onClose')(this.get('key'));
+      }
     }
+  }
 });
