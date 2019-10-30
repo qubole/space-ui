2bd74523376b96122264ac6be4382f3c2cf8c438
Aaditya Sharma <asharma@qubole.com>
2019-10-30 11:40:51 +0530

    Added colors

--- addon/components/s-tag.js
+++ addon/components/s-tag.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
+import layout from '../templates/components/s-tag';
 
 export default Component.extend({
+  layout,
   tagName: 'span',
   classNames: ['s-tag', 'container', 'mr5', 'fs16'],
   classNameBindings: ['getType'],
