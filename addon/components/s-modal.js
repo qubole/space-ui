1b37a7e8e8ded3f335f17280b5b6d91a9441b2ee
Manzoor Husain <manzoor@qubole.com>
2019-11-28 14:12:14 +0530

    fix model close

--- addon/components/s-modal.js
+++ addon/components/s-modal.js
@@ -3,6 +3,15 @@ import layout from '../templates/components/s-modal';
 
 export default Component.extend({
     layout,
-    classNames: ['s-modal']
+    classNames: ['s-modal'],
+    isVisible: true,
+    actions: {
+        onCancel() {
+            this.set('isVisible', false);
+            if (this.get('onCancelAction')) {
+                this.get('onCancelAction')();
+            }
+        }
+    }
 
 });
