dcae04df944228629d167b2816be673c8e3bd1e1
Aaditya Sharma <asharma@qubole.com>
2020-02-29 02:17:46 +0530

    Modified s-modal

--- addon/components/s-modal.js
+++ addon/components/s-modal.js
@@ -1,8 +1,18 @@
 import Component from '@ember/component';
 import layout from '../templates/components/s-modal';
+import {
+  computed
+} from '@ember/object';
 
 export default Component.extend({
-    layout,
-    classNames: ['s-modal']
+  layout,
+  classNames: ['s-modal'],
+  actionPassed: computed('onCancel', 'onOK', function(){
+    if(this.get('onCancel') || this.get('onOK')){
+      return true;
+    }
+    return false;
+  })
+
 
 });
