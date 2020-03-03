dc80c33606d5c62b5923dda83e6d64d86c037411
Aaditya Sharma <aadityaja@gmail.com>
2020-03-03 12:58:25 +0530

    Hide footer when no action is passed to s-modal (#11)

--- addon/components/s-modal.js
+++ addon/components/s-modal.js
@@ -1,10 +1,19 @@
 import Component from '@ember/component';
 import layout from '../templates/components/s-modal';
+import {
+  computed
+} from '@ember/object';
 
 export default Component.extend({
   layout,
   classNames: ['s-modal'],
   isVisible: true,
+  showFooter: computed('onCancel', 'onOK', function(){
+    if (this.get('onCancel') || this.get('onOK')) {
+      return true;
+    }
+    return false;
+  }),
   actions: {
     onCancelAction() {
       this.set('isVisible', false);
