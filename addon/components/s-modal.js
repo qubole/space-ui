611d1beee063c5b08b0cc6608f2264b0f0ce33ee
Aaditya Sharma <aadityaja@gmail.com>
2020-03-03 12:23:59 +0530

    Added cancel button on s-modal (#10)

--- addon/components/s-modal.js
+++ addon/components/s-modal.js
@@ -2,7 +2,16 @@ import Component from '@ember/component';
 import layout from '../templates/components/s-modal';
 
 export default Component.extend({
-    layout,
-    classNames: ['s-modal']
+  layout,
+  classNames: ['s-modal'],
+  isVisible: true,
+  actions: {
+    onCancelAction() {
+      this.set('isVisible', false);
+      if (this.get('onCancel')) {
+        this.get('onCancel')();
+      }
+    }
+  }
 
 });
