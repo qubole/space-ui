68e193dc763940e42ae7dc5e6d08ee4b6334443d
Abhishek Upadhyay <aupadhyay@qubole.com>
2020-04-03 19:14:37 +0530

    adding className bindings for title section to have that class when clicked.

--- addon/components/s-timeline/event/content/title.js
+++ addon/components/s-timeline/event/content/title.js
@@ -4,8 +4,11 @@ import layout from 'space-ui/templates/components/s-timeline/event/content/title
 export default Component.extend({
     layout,
     classNames: ['s-timeline-event-content-title'],
+    classNameBindings: ['isActive:active'],
+    isActive:false,
     actions: {
         toggleEvent(index) {
+            this.set('isActive', !this.get('isActive'));
             this.toggleEvent(index);
         }
     }
